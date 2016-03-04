70e8259c0ec583865d0f4c0ada919b90827679dd
Andrew Suzuki <andrew.b.suzuki@gmail.com>
2016-03-03 22:50:09 -0500

    correct exporting

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import $script from 'scriptjs'
 
 $script('//platform.twitter.com/widgets.js', 'twitter-widgets')
 
-export * as Follow from './components/Follow'
-export * as Hashtag from './components/Hashtag'
-export * as Mention from './components/Mention'
-export * as Share from './components/Share'
-export * as Timeline from './components/Timeline'
-export * as Tweet from './components/Tweet'
+export Follow from './components/Follow'
+export Hashtag from './components/Hashtag'
+export Mention from './components/Mention'
+export Share from './components/Share'
+export Timeline from './components/Timeline'
+export Tweet from './components/Tweet'
