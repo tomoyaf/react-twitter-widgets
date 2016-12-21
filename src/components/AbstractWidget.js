54dbdd252d38b8fa7e641f6a37c96dd829a0fb75
Mike Lambert <mlambert@gmail.com>
2016-12-20 22:44:41 -0500

    Prevent unnecessary JS exceptions offline.

--- src/components/AbstractWidget.js
+++ src/components/AbstractWidget.js
@@ -26,6 +26,13 @@ export default class AbstractWidget extends React.Component {
     const $script = require('scriptjs')
 
     $script.ready('twitter-widgets', () => {
+      if (!window.twttr) {
+        // If the script tag fails to load, scriptjs.ready() will still trigger.
+        // Let's avoid the JS exceptions when that happens.
+        console.error('Failure to load window.twttr, aborting load.');
+        return;
+      }
+
       // Delete existing
       this.removeChildren(widgetWrapper)
 
