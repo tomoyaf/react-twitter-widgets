91d9c406e19ed830612f1d68e801be8645de7738
Andrew Suzuki <andrew.b.suzuki@gmail.com>
2017-04-25 11:17:32 -0400

    Auto-generate code examples for widget examples (/site)

--- site/src/WidgetExample.js
+++ site/src/WidgetExample.js
@@ -23,15 +23,35 @@ const widgetNameToComponentMap = {
 }
 
 
+const jsonPropertyQuoteRegex = /"([^(")"]+)":/g
+
+
 export default class WidgetExample extends Component {
   render() {
     const { widget, widgetProps } = this.props
 
     const c = widgetNameToComponentMap[widget] || 'div'
 
+    // Generate a string of jsx-like props for
+    // code example based on on widgetProps
+    const propsString = Object.keys(widgetProps).map((wpn) => {
+      const v = widgetProps[wpn]
+
+      const displayValue = (typeof v === 'string')
+        ? `"${v}"`
+        // json-stringify props, then remove quotes from property names
+        : `{${JSON.stringify(widgetProps[wpn]).replace(jsonPropertyQuoteRegex, '$1:')}}`
+
+      return `  ${wpn}=${displayValue}`
+    }).join('\n')
+
+    const exampleCode = `<${widget}\n${propsString}\n/>`
+
     return (
       <div className="WidgetExample-container">
         <h3>{widget}</h3>
+        <pre>{exampleCode}</pre>
+        <br /><br />
         {React.createElement(c, widgetProps)}
       </div>
     )
