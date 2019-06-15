032b154eb9457805fabefaa5dbc00ae92a7d67cd
tomoyaf <gazimum@gmail.com>
2019-06-15 20:44:23 +0900

    Add style

--- src/components/AbstractWidget.js
+++ src/components/AbstractWidget.js
@@ -1,15 +1,16 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
 export default class AbstractWidget extends React.Component {
   static propTypes = {
     ready: PropTypes.func.isRequired,
+    customStyle: PropTypes.object
   };
 
   static removeChildren(node) {
     if (node) {
       while (node.firstChild) {
-        node.removeChild(node.firstChild)
+        node.removeChild(node.firstChild);
       }
     }
   }
@@ -17,57 +18,58 @@ export default class AbstractWidget extends React.Component {
   static removeChildrenExceptLast(node) {
     if (node) {
       while (node.childNodes.length > 1) {
-        node.removeChild(node.firstChild)
+        node.removeChild(node.firstChild);
       }
     }
   }
 
   componentWillMount() {
-    this.willUnmount = false
+    this.willUnmount = false;
   }
 
   componentDidMount() {
-    this.loadWidget()
+    this.loadWidget();
   }
 
   componentDidUpdate() {
-    this.loadWidget()
+    this.loadWidget();
   }
 
   componentWillUnmount() {
-    this.willUnmount = true
+    this.willUnmount = true;
   }
 
   loadWidget = () => {
-    const $script = require('scriptjs') // eslint-disable-line global-require
+    const $script = require("scriptjs"); // eslint-disable-line global-require
 
-    $script.ready('twitter-widgets', () => {
+    $script.ready("twitter-widgets", () => {
       if (!window.twttr) {
         // If the script tag fails to load, scriptjs.ready() will still trigger.
         // Let's avoid the JS exceptions when that happens.
-        console.error('Failure to load window.twttr, aborting load.') // eslint-disable-line no-console
-        return
+        console.error("Failure to load window.twttr, aborting load."); // eslint-disable-line no-console
+        return;
       }
 
       // Delete existing
-      AbstractWidget.removeChildren(this.widgetWrapper)
+      AbstractWidget.removeChildren(this.widgetWrapper);
 
       // Create widget
-      this.props.ready(window.twttr, this.widgetWrapper, this.done)
-    })
-  }
+      this.props.ready(window.twttr, this.widgetWrapper, this.done);
+    });
+  };
 
   done = () => {
     if (this.willUnmount) {
-      AbstractWidget.removeChildrenExceptLast(this.widgetWrapper)
+      AbstractWidget.removeChildrenExceptLast(this.widgetWrapper);
     }
-  }
+  };
 
   render() {
-    return React.createElement('div', {
-      ref: (c) => {
-        this.widgetWrapper = c
+    return React.createElement("div", {
+      ref: c => {
+        this.widgetWrapper = c;
       },
-    })
+      style: this.props.customStyle
+    });
   }
 }
